b61c543f8ef1ab61bc5d2784320e404b6039a639
Get-Ryan <ryan.simeone@gmail.com>
2015-08-01 16:25:36 -0500

    Update Public-ADPasswordExpirationNotice.ps1

--- Public-ADPasswordExpirationNotice.ps1
+++ Public-ADPasswordExpirationNotice.ps1
@@ -1,12 +1,11 @@
-﻿<#
+<#
 .Description
  This script obtains AD accounts with passwords nearing expiration and sends notification emails on certain days prior to expiration.
- Currently, it only sends emails if the password age matches the exact date-frame out from expiration.
+ Currently, it only sends emails if the password age matches the exact date frame out from expiration.
  There is no check to see if an account was missed by the script not being run on a particular day.
 .Notes
  AUTHOR: Ryan Simeone
- LASTEDIT: 7/29/2015 3:20 PM UTC
-
+ LASTEDIT: 7/30/2015 5:15 PM UTC
 #>
 
 $ErrorActionPreference = "Stop"
@@ -26,7 +25,7 @@ Function CheckPasswordAge() {
 
 ### List Accounts with PasswordLastSet equal to or older than max password age +14 days in the past
 
-    $Accounts = Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False -and PasswordLastSet -gt 0} –Properties * | where {($_.PasswordLastSet) -lt $14days} | Sort-Object PasswordLastSet
+    $Accounts = Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False -and PasswordLastSet -gt 0} –Properties * | where {($_.PasswordLastSet) -le $14days} | Sort-Object PasswordLastSet
 
 ### Determine accounts with password ages at previously set timeframes and email reminders to change their password.
     
@@ -58,7 +57,27 @@ Function SendNotificationEmail([string]$TimeframeString) {
 
 ### Send email with the specified timeframe to the user
 
-    Send-MailMessage -SMTPServer SMTPServer.Domain.com -To $Account.UserPrincipalName -From Address@Domain.com -Cc Address@Domain.com -Subject "Password Notice - Your password will expire $($TimeframeString)" -BodyAsHtml "Hello $($Account.GivenName),<BR><BR>This is an automated reminder that your password will be expiring $($TimeframeString) on $($Account.PasswordLastSet.AddDays($maxPwdAge).ToShortDateString()).<BR>You can press Ctrl+Alt+Del to bring up a screen where you can change your password.<BR><BR>Sincerely,<BR>The <Company> IT Team"
+$EmailBody = @"
+Hello $($Account.GivenName),<BR>
+<BR>
+This is an automated reminder that your password will be expiring $($TimeframeString) 
+on $($Account.PasswordLastSet.AddDays($maxPwdAge).Date.ToShortDateString()).<BR>
+You can press Ctrl+Alt+Del to bring up a screen where you can change your password.<BR>
+<BR>
+Sincerely,<BR>
+The <company> IT Team"
+"@
+
+$MessageParams = @{
+        To = $Account.UserPrincipalName
+        SMTPServer = "Server.Domain.com"
+        From = "Address@Domain.com"
+        Subject = "Password Notice - Your password will expire $($TimeframeString)"
+        BodyAsHtml = $true
+        Body = $EmailBody
+        }
+
+Send-MailMessage @MessageParams
         
 }
 
